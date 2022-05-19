6186227931daa8cb4323be9fb195e05d7912c4c0
Arman Kiani <armankianipm@gmail.com>
2022-05-19 22:39:01 +0430

    ?!

---
+++ db.js
@@ -0,0 +1,35 @@
+let db = {}
+db.users = [{
+    "id": "1",
+    "firstname": "user",
+    "lastname": "user",
+    "email": "uuser@example.com",
+    "born_date": "17.06.2020",
+    "password": "123123",
+    "phone_number": "123123123",
+    "description": "firstdescription"
+},
+{
+    "id": "2",
+    "firstname": "user2",
+    "lastname": "user2",
+    "email": "uuser3@example.com",
+    "born_date": "17.06.2021",
+    "password": "111111",
+    "phone_number": "123123123",
+    "description": "firstdescription"
+},]
+
+
+db.users.push({userName: 'armank',pass:'paas', name: 'arman'})
+
+return db.users
+
+User logged in
+Username or password not correct.
+
+
+export default function sum(x, y) {
+    return x + y
+  }
+  
--- server.js
+++ server.js
@@ -1,5 +1,33 @@
 'use strict';
 const express = require('express');
+//const db = require('db.js');
+let db = {}
+db.users = [{
+    "id":1,
+    "userName": "user",
+    "name": "user",
+    "pass": "123123",
+},
+{
+    "id":2,
+    "userName": "user2",
+    "name": "user2",
+    "pass": "111111",
+},]
+
+db.products = [{
+    "id":100,
+    "productName": "product1",
+    "productDescription": "product Description1",
+    "productPrice": 123123,
+},
+{
+    "id":102,
+    "productName": "product2",
+    "productDescription": "product Description2",
+    "productPrice": 123123,
+},]
+
 
 //constants
 const PORT = 8080;
@@ -7,19 +35,30 @@ const HOST = '0.0.0.0';
 
 //App
 const app = express();
+
+
 app.get('/', (req, res) => {
-    res.send('Hello World.!');
+    res.send('Hello World.!',req,res);
+    console.log(db);
+
 });
 
 app.get('/users', (req, res) => {
-    res.send(' a collection of registered usres');
+    for (let index = 0; index < db.users.length; index++) {
+        res.send(db.users.id[index]+" " + db.users.userName[index]+" " + db.users.name[index]+" " + db.users.pass[index]);
+    }
 });
+
 app.get('/users/:id', (req, res) => {
     res.send('get a user by user id');
 });
 app.post('/users', (req, res) => {
+    db.users.push({userName: 'armank',pass:'paas', name: 'arman',id:3})
     res.send('register a user');
+    console.log(db);
+
 });
+
 app.get('/products', (req, res) => {
     res.send('collection of registered products');
 });
@@ -27,15 +66,13 @@ app.get('/products/:id', (req, res) => {
     res.send('get a product by product id');
 });
 app.post('/products', (req, res) => {
+    db.products.push({productName: 'product3',productPrice:3214, productDescription: 'productDecription3',id:3})
     res.send('register a product');
 });
+
 app.post('/login', (req, res) => {
     res.send('authenticate the user');
 });
 
-
-
-
 app.listen(PORT,HOST);
 console.log('Running on http://${HOST}:${PORT}');
-
