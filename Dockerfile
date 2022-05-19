568fbd91e1eda354db27df0e592729511f976d6a
Arman Kiani <armankianipm@gmail.com>
2022-05-19 18:22:05 +0430

    شیی ًخعفث

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM node:16
 # Create app directory
-WORKDIR /home/arman/Projects/DockerizingNode.js
+WORKDIR /usr/src/app
 
 # Install app dependencies
 # A wildcard is used to ensure both package.json AND package-lock.json are copied
--- server.js
+++ server.js
@@ -11,6 +11,27 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+app.get('/users', (req, res) => {
+    res.send(' a collection of registered usres');
+});
+app.get('/users/:id', (req, res) => {
+    res.send('get a user by user id');
+});
+app.post('/users', (req, res) => {
+    res.send('register a user');
+});
+app.get('/products', (req, res) => {
+    res.send('collection of registered products');
+});
+app.get('/products/:id', (req, res) => {
+    res.send('get a product by product id');
+});
+app.post('/products', (req, res) => {
+    res.send('register a product');
+});
+app.post('/login', (req, res) => {
+    res.send('authenticate the user');
+});
 app.listen(PORT,HOST);
 console.log('Running on http://${HOST}:${PORT}');
 
