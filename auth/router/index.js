fe5949ecb9243b7e4cf8f5f673d373510676d978
Arman Kiani <armankianipm#gmail.com>
2022-06-13 09:22:54 +0430

    git commit -m "Resolved merge conflict"

--- auth/router/index.js
+++ auth/router/index.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const authRouter = express.Router();
-const { login, welcome, refresh, register } = require("../handlers");
+const {
+  login,
+  welcome,
+  refresh,
+  register,
+  gitCallback,
+  gitSuccess,
+  auth,
+  github,
+} = require("../handlers");
 
 authRouter.post("/auth/register", register);
 authRouter.post("/auth/login", login);
 authRouter.get("/auth/welcome", welcome);
 authRouter.post("/auth/refresh", refresh);
+authRouter.get("/auth/github/callback", gitCallback);
+authRouter.get("/auth/success", gitSuccess);
+authRouter.get("/auth", auth);
+authRouter.get("/auth/github", github);
 
 module.exports = authRouter;
